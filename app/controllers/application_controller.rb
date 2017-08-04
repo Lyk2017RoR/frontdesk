03c649950512b8300b6f57edb2cfce4b97627aba
eneskilicaslan <eeneskilicaslan@gmail.com>
2017-08-04 15:32:14 +0300

    User Authentication with Devise Gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception # prepend: true !! this might be added later
 end
