3c986d054213ec6609e26a094a137f4e05d03fb3
Umut SİNAV <umutsinav@gmail.com>
2017-08-05 03:26:38 +0300

    Product controller show
    Show metodu yazıldı
    show view oluşturuldu
    testleri yapıldıı

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,6 +19,10 @@ class ProductsController < ApplicationController
     end
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
   private
   def product_params
     params.require(:product).permit(:title, :content, :category_id, :brand_id)
