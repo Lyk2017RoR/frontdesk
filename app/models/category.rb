3709b3e856394afa3a00f12b36aa1f604c8bfd72
mali44 <maalogy@gmail.com>
2017-08-04 23:47:22 -0700

    AS

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,21 +1,4 @@
-# == Schema Information
-#
-# Table name: categories
-#
-#  id         :integer          not null, primary key
-#  title      :string
-#  slug       :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
 class Category < ApplicationRecord
-  has_many :products
 
-  validates :title, presence:  true
-  validate :slug_parameterize
 
-  def slug_parameterize
-    self.slug = title.parameterize
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,31 +1,11 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id                     :integer          not null, primary key
-#  email                  :string
-#  encrypted_password     :string           default(""), not null
-#  reset_password_token   :string
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  sign_in_count          :integer          default(0), not null
-#  current_sign_in_at     :datetime
-#  last_sign_in_at        :datetime
-#  current_sign_in_ip     :string
-#  last_sign_in_ip        :string
-#  username               :string
-#  password               :string
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
-#
-
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable
 
-  has_one :profile, inverse_of: :user
-  enum role: %i[client admin]
+  
+
+
 
 end
