9051cc98063824fb5920cff33b7686263ab145be
Umut SİNAV <umutsinav@gmail.com>
2017-08-05 12:29:49 +0300

    yayın fixleri

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'static_pages#welcome'
+  root to: 'products#index'
   resources :brands
   resources :categories
   resources :products
-  
+
 
 
 end
