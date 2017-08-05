fb14b6c5778f7cda2e2c275015f8dff80fd180a4
Umut SİNAV <umutsinav@gmail.com>
2017-08-05 03:24:49 +0300

    Product oluşturma
    create ve new methodları ile product oluşturuldu.
    strong parametreler ayarlandı

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,5 +6,21 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
+    @categories = Category.all.map { |c| [c.title, c.id] }
+    @brands = Brand.all.map { |c| [c.title, c.id] }
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:title, :content, :category_id, :brand_id)
   end
 end
