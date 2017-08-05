35bc7036c9330bab4038d4e330d9dde25bcc1a0b
Umut SİNAV <umutsinav@gmail.com>
2017-08-05 10:27:48 +0300

    Product migration price
    Product price kolonu eklendi. db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170804133254) do
+ActiveRecord::Schema.define(version: 20170805071555) do
 
   create_table "brands", force: :cascade do |t|
     t.string "title"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20170804133254) do
     t.integer "brand_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "price"
     t.index ["brand_id"], name: "index_products_on_brand_id"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
